7352f37c4c440444254f771620785608b335bf7f
risaldi1508 <75102472+risaldi1508@users.noreply.github.com>
2020-12-11 10:39:56 +0800

    FINAL GAME
    Untuk fitur OOP yang tercantum di dalam scrip adalah Encapculation yang terdapat pada baris 16-22 dan 104-106

--- battleship.java
+++ battleship.java
@@ -1,43 +1,43 @@
 import java.util.*;
 
     public class battleship {
-        public static int numRows = 10;
-        public static int numCols = 10;
+        public static int numRows = 5;
+        public static int numCols = 5;
         public static int playerShips;
         public static int computerShips;
         public static String[][] grid = new String[numRows][numCols];
         public static int[][] missedGuesses = new int[numRows][numCols];
 
         public static void main(String[] args){
-            System.out.println("**** Selamat Datang Di Game PEMBUNUHAN ****");
+            System.out.println("**** Selamat Datang Di Game kapal tembak ****");
             System.out.println("SEKARANG LAUT KOSONG\n");
 
             //Step 1 – buat peta
-            buatpeta();
+            createOceanMap();
 
             //Step 2 – Sebarkan kapal pemain
-            sebarkankapalpemain();
+            deployPlayerShips();
 
-            //Step 3 - menyebarkan kapal lawan
-            sebarkankapallawan();
+            //Step 3 - menyebarkan kapal komputer
+            deployComputerShips();
 
             //Step 4 pertarungan
             do {
-                pertarungan();
+                Battle();
             }while(battleship.playerShips != 0 && battleship.computerShips != 0);
 
             //Step 5 - Game over
             gameOver();
         }
 
-        public static void buatpeta(){
-            //First section of Ocean Map
+        public static void createOceanMap(){
+            //Bagian pertama dari peta laut
             System.out.print("  ");
             for(int i = 0; i < numCols; i++)
                 System.out.print(i);
             System.out.println();
 
-            //Middle section of Ocean Map
+            //Bagian tengan peta laut
             for(int i = 0; i < grid.length; i++) {
                 for (int j = 0; j < grid[i].length; j++) {
                     grid[i][j] = " ";
@@ -58,16 +58,16 @@ import java.util.*;
             System.out.println();
         }
 
-        public static void sebarkankapalpemain(){
+        public static void deployPlayerShips(){
             Scanner input = new Scanner(System.in);
 
-            System.out.println("\nSebarkan kapal lawan");
-            //Menyebarkan lima kapal untuk pemain
+            System.out.println("\n  Sebarkan kapal anda");
+            //Mengerahkan lima kapal untuk pemain
             battleship.playerShips = 5;
             for (int i = 1; i <= battleship.playerShips; ) {
-                System.out.print("Masukkan koordinat x" + i + " Kapal: ");
+                System.out.print("Masukkan titik koordinat" + i + " kapal: ");
                 int x = input.nextInt();
-                System.out.print("Masukkan koordinat y" + i + " Kapal: ");
+                System.out.print("Masukkan titik koordinat" + i + " kapal: ");
                 int y = input.nextInt();
 
                 if((x >= 0 && x < numRows) && (y >= 0 && y < numCols) && (grid[x][y] == " "))
@@ -76,16 +76,16 @@ import java.util.*;
                     i++;
                 }
                 else if((x >= 0 && x < numRows) && (y >= 0 && y < numCols) && grid[x][y] == "@")
-                    System.out.println("Anda tidak dapat menempatkan dua atau lebih kapal di lokasi yang sama");
+                    System.out.println("Anda tidak boleh menempatkan dua atau lebih kapal di lokasi yang sama");
                 else if((x < 0 || x >= numRows) || (y < 0 || y >= numCols))
-                    System.out.println("Anda tidak dapat menempatkan kapal di luar " + numRows + " by " + numCols + " grid");
+                    System.out.println("Anda tidak dapat menempatkan kapal di luar " + numRows + " dari " + numCols + " grid");
             }
             printOceanMap();
         }
 
-        public static void sebarkankapallawan(){
-            System.out.println("\nlawan menyebarkan kapal");
-            //Menyebarkan 5 kapal untuk lawan
+        public static void deployComputerShips(){
+            System.out.println("\nLawan sedang menyebarkan kapal");
+            //Penyebaran 5 kapal lawan
             battleship.computerShips = 5;
             for (int i = 1; i <= battleship.computerShips; ) {
                 int x = (int)(Math.random() * 10);
@@ -101,100 +101,100 @@ import java.util.*;
             printOceanMap();
         }
 
-        public static void pertarungan(){
+        public static void Battle(){
             playerTurn();
             computerTurn();
 
             printOceanMap();
 
             System.out.println();
-            System.out.println("Kapal Anda: " + battleship.playerShips + " | Kapal Lawan: " + battleship.computerShips);
+            System.out.println("Kapal kamu: " + battleship.playerShips + " | Kapl lawan: " + battleship.computerShips);
             System.out.println();
         }
 
         public static void playerTurn(){
-            System.out.println("\nGILIRANMU");
+            System.out.println("\nGILIRAN KAMU");
             int x = -1, y = -1;
             do {
                 Scanner input = new Scanner(System.in);
                 System.out.print("Masukkan koordinat x: ");
                 x = input.nextInt();
-                System.out.print("Masukkan koordinat y: ");
+                System.out.print("Masukkan koordinat Y: ");
                 y = input.nextInt();
 
-                if ((x >= 0 && x < numRows) && (y >= 0 && y < numCols)) //valid guess
+                if ((x >= 0 && x < numRows) && (y >= 0 && y < numCols)) //TEBAKAN YANG VALID
                 {
-                    if (grid[x][y] == "x") //if computer ship is already there; computer loses ship
+                    if (grid[x][y] == "x") //Jika tebakan benar, lawan kehilangan kapal
                     {
-                        System.out.println("Ledakan, anda menenggelamkan kapal!");
-                        grid[x][y] = "!"; //Hit mark
+                        System.out.println("yeahhh! Anda menenggelamkan kapal");
+                        grid[x][y] = "!"; //tanda Hit
                         --battleship.computerShips;
                     }
                     else if (grid[x][y] == "@") {
-                        System.out.println("maaf kamu menembak kapal sendiri :(");
-                        grid[x][y] = "x";1
+                        System.out.println("ouhh salah bro, kamu menenggelamkan kapal sendiri :(");
+                        grid[x][y] = "x";
                         --battleship.playerShips;
-                        ++battleship.computerShips;
+                        --battleship.computerShips;
                     }
                     else if (grid[x][y] == " ") {
-                        System.out.println("Tembakan anda meleset, coba lagi");
+                        System.out.println("Maaf, Kamu terlewatkan");
                         grid[x][y] = "-";
                     }
                 }
-                else if ((x < 0 || x >= numRows) || (y < 0 || y >= numCols))  //invalid guess
-                    System.out.println("anda tidak bisa menempatkan kapal " + numRows + " di luar " + numCols + " area");
-            }while((x < 0 || x >= numRows) || (y < 0 || y >= numCols));  //keep re-prompting till valid guess
+                else if ((x < 0 || x >= numRows) || (y < 0 || y >= numCols))  //Tebakan tidak valid
+                    System.out.println("Kamu tidak boleh menyebarkan kapal di luar " + numRows + " dari " + numCols + " grid");
+            }while((x < 0 || x >= numRows) || (y < 0 || y >= numCols));  //Terus meminta sampai tebakan valid
         }
 
         public static void computerTurn(){
-            System.out.println("\nCOMPUTER'S TURN");
-            //Guess co-ordinates
+            System.out.println("\nGILIRAN LAWAN");
+            //tebak untuk berkoordinasi
             int x = -1, y = -1;
             do {
                 x = (int)(Math.random() * 10);
                 y = (int)(Math.random() * 10);
 
-                if ((x >= 0 && x < numRows) && (y >= 0 && y < numCols)) //valid guess
+                if ((x >= 0 && x < numRows) && (y >= 0 && y < numCols)) //tebakan valid
                 {
-                    if (grid[x][y] == "@") //if player ship is already there; player loses ship
+                    if (grid[x][y] == "@") //jika kapal pemain ditembak, pemain kehilangan kapal
                     {
-                        System.out.println("The Computer sunk one of your ships!");
+                        System.out.println("lawan menenggelamkan kapal!");
                         grid[x][y] = "x";
                         --battleship.playerShips;
-                        ++battleship.computerShips;
+                        --battleship.computerShips;
                     }
                     else if (grid[x][y] == "x") {
-                        System.out.println("The Computer sunk one of its own ships");
+                        System.out.println("Lawan menenggelamkan salah satu kapalnya sendiri");
                         grid[x][y] = "!";
                     }
                     else if (grid[x][y] == " ") {
-                        System.out.println("Computer missed");
-                        //Saving missed guesses for computer
+                        System.out.println("lawan terlewatkan");
+                        //menyimpan tebakan yang terlewat untuk lawan
                         if(missedGuesses[x][y] != 1)
                             missedGuesses[x][y] = 1;
                     }
                 }
-            }while((x < 0 || x >= numRows) || (y < 0 || y >= numCols));  //keep re-prompting till valid guess
+            }while((x < 0 || x >= numRows) || (y < 0 || y >= numCols));  //terus meminta sampai tebakan valid
         }
 
         public static void gameOver(){
             System.out.println("Your ships: " + battleship.playerShips + " | Computer ships: " + battleship.computerShips);
             if(battleship.playerShips > 0 && battleship.computerShips <= 0)
-                System.out.println("Hooray! You won the battle :)");
+                System.out.println("yeahhh! kamu menang :)");
             else
-                System.out.println("Sorry, you lost the battle");
+                System.out.println("maaf kamu kalah");
             System.out.println();
         }
 
         public static void printOceanMap(){
             System.out.println();
-            //First section of Ocean Map
+            //Bgian pertama dari peta laut
             System.out.print("  ");
             for(int i = 0; i < numCols; i++)
                 System.out.print(i);
             System.out.println();
 
-            //Middle section of Ocean Map
+            //Bagian tengah dari peta laut
             for(int x = 0; x < grid.length; x++) {
                 System.out.print(x + "|");
 
@@ -205,12 +205,10 @@ import java.util.*;
                 System.out.println("|" + x);
             }
 
-            //Last section of Ocean Map
+            //bagian terakhir dari peta laut
             System.out.print("  ");
             for(int i = 0; i < numCols; i++)
                 System.out.print(i);
             System.out.println();
         }
     }
-
-
